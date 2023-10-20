40d3a862c3835cb0dfacf04c1f5b27a4b806757d
Tristian Sellers <tristianesellers@gmail.com>
2023-10-20 11:05:55 -0500

    currently testing neg nums, fixed lgoic errors and added unit tests for .getNumerator() & .getDenominator() & .hashCode() & .equals()

--- src/Fraction.java
+++ src/Fraction.java
@@ -4,8 +4,33 @@ public class Fraction {
     private int denominator;
 
     public Fraction(int top, int bottom) {
-        numerator = top;
-        denominator = bottom;
+        if (top == 0 || bottom == 0) {
+            numerator = 0;
+            denominator = 1;
+        }
+        else if (top < 0 || bottom < 0) {
+            if (bottom < 0) {
+                numerator = top * -1;
+                denominator = bottom * -1;
+            }
+        }
+        else {
+            int gcd = find_gcd(top, bottom);
+            numerator = top / gcd;
+            denominator = bottom / gcd;
+        }
+    }
+
+    private int find_gcd(int a, int b) {
+        while (a != b) {
+            if (a > b) {
+                a -= b;
+            }
+            else if (b > a){
+                b -= a;
+            }
+        }
+        return a;
     }
 
     public int getNumerator() {
--- src/FractionTest.java
+++ src/FractionTest.java
@@ -2,8 +2,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FractionTest {
 
-    int [] test_numerators = {0, 1, 4, 7, 18, 9};
-    int [] test_denominators = {1, 2, 7, 11, 2, 9};
+    int []   test_numerators = {0, 1, 4, 7, 18, 9, -1, 3, -9};
+    int [] test_denominators = {1, 2, 7, 11, 2, 9, 10, -5, -3};
+
+    int []   expected_numerators = {0, 1, 4, 7, 9, 1, -1, -3, 3};
+    int [] expected_denominators = {1, 2, 7, 11, 1, 1, 10, 5, 1};
 
     Fraction [] test_fractions;
 
@@ -12,6 +15,8 @@ class FractionTest {
         assert test_numerators.length == test_denominators.length;
         test_fractions = new Fraction [test_numerators.length];
         for (int i = 0; i < test_numerators.length; i++) {
+//            System.out.println("Numerator: " + test_numerators[i]);
+//            System.out.println("Denominator: " + test_denominators[i]);
             test_fractions[i] = new Fraction(test_numerators[i], test_denominators[i]);
         }
     }
@@ -23,18 +28,53 @@ class FractionTest {
 
     @org.junit.jupiter.api.Test
     void getNumerator() {
+        assert expected_numerators.length == test_fractions.length;
+        for (int i = 0; i < test_fractions.length; i++) {
+            Fraction f = test_fractions[i];
+            int expected_numerator = expected_numerators[i];
+            int actual_numerator = f.getNumerator();
+            assertEquals(expected_numerator, actual_numerator);
+        }
     }
 
     @org.junit.jupiter.api.Test
     void getDenominator() {
+        assert expected_denominators.length == test_fractions.length;
+        int [] actual_denominators = new int [test_fractions.length];
+        for (int i = 0; i < test_fractions.length; i++) {
+            actual_denominators[i] = test_fractions[i].getDenominator();
+        }
+        assertArrayEquals(expected_denominators, actual_denominators);
     }
 
     @org.junit.jupiter.api.Test
     void testEquals() {
+        for (int i = 0; i < test_fractions.length; i++) {
+            Fraction f = test_fractions[i];
+            Fraction f_equiv = f;
+            assertEquals(f, f_equiv);
+            assertNotEquals(f, null);
+            assertNotEquals(f, "not a fraction");
+
+            Fraction equals_f = new Fraction(test_numerators[i], test_denominators[i]);
+            assertNotSame(f, equals_f); // if (f != equals_f)
+            assertEquals(f, equals_f);
+
+            Fraction not_equals_f = new Fraction(1 + test_numerators[i], test_denominators[i]);
+            assertNotEquals(f, not_equals_f);
+        }
     }
 
     @org.junit.jupiter.api.Test
     void testHashCode() {
+        for (int i = 0; i < test_fractions.length; i++) {
+            Fraction f = test_fractions[i];
+            Fraction equals_f = new Fraction(test_numerators[i], test_denominators[i]);
+            assertNotSame(f, equals_f);
+            assertEquals(f, equals_f);
+            Fraction not_equals_f = new Fraction (test_numerators[1] + 1, test_denominators[i]);
+            assertNotEquals(f, not_equals_f);
+        }
     }
 
     @org.junit.jupiter.api.Test
